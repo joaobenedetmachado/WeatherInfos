d93500409e114557bb752ddc7ba6f14dde00c185
João Benedet <105388117+joaobenedetmachado@users.noreply.github.com>
2024-02-10 19:54:07 -0300

    file pro api

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import axios from 'axios';
 import WeatherInfos from './Components/weatherinfos/weatherinfos';
 import InputSearch from './Components/inputSearch/inputSearch';
 
-const api = {
-  key: "24897893e27c26a4a999b9250bbc38d0",
-  base: "https://api.openweathermap.org/data/2.5/",
-  lang: "pt_br",
-  units: "metric"
-}
+
 
 function getUsers(text) {
   const url = `${api.base}weather?q=${text}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`;
---
+++ src/config_api.js
@@ -0,0 +1,6 @@
+const api = {
+    key: "24897893e27c26a4a999b9250bbc38d0",
+    base: "https://api.openweathermap.org/data/2.5/",
+    lang: "pt_br",
+    units: "metric"
+  }
